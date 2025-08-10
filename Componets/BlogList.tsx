1eb1534c8dd4f30c14946b58ce23ab7d92cefb6a
John <johnedokpolor@gmail.com>
2025-08-10 14:41:12 +0100

    added animations

--- Componets/BlogList.tsx
+++ Componets/BlogList.tsx
@@ -1,10 +1,13 @@
+"use client";
 import { blog_data } from "@/Assets/assets";
-import React from "react";
+import React, { useState } from "react";
 import BlogItem from "./Blogitem";
-
+import { motion } from "motion/react";
+import { div } from "motion/react-client";
 const BlogList = () => {
+  const [menu, setMenu] = useState("All");
   const categories = [
-    // { name: "All" },
+    { name: "All" },
     { name: "Technology" },
     { name: "Lifestyle" },
     { name: "Education" },
@@ -15,24 +18,57 @@ const BlogList = () => {
   return (
     <div>
       <div className="flex flex-wrap justify-center my-10">
-        <button className="px-4 py-1 text-white bg-black rounded-sm">
-          All{" "}
-        </button>
-        {categories.map((category) => (
-          <button className="px-4 py-1 text-black rounded-sm">
+        {categories.map((category, i) => (
+          <motion.button
+            initial={{ opacity: 0, y: -50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: i * 0.1 }}
+            key={i}
+            onClick={() => setMenu(category.name)}
+            className={`px-4 py-1 text-black rounded-sm ${
+              menu === category.name && "text-white bg-black"
+            }`}
+          >
             {category.name}
-          </button>
+          </motion.button>
         ))}
       </div>
+
       <div className="flex flex-wrap justify-around mb-16 gap-y-10 sm:mx-10 ">
-        {blog_data.map((blog) => {
+        {blog_data.map((blog, i) => {
           return (
-            <BlogItem
-              title={blog.title}
-              description={blog.description}
-              category={blog.category}
-              image={blog.image}
-            />
+            <motion.div
+              initial={{ opacity: 0, x: -50, rotate: -10 }}
+              whileInView={{
+                opacity: 1,
+                x: 0,
+                rotate: 0,
+
+                transition: {
+                  duration: 0.5,
+                  delay: i * 0.1,
+                },
+              }}
+              whileHover={{
+                scale: 1.05,
+                rotate: 1,
+                borderRadius: "50%",
+              }}
+              transition={{
+                duration: 0.2,
+                type: "spring",
+                stiffness: 300,
+                damping: 10,
+              }}
+            >
+              <BlogItem
+                key={i}
+                title={blog.title}
+                description={blog.description}
+                category={blog.category}
+                image={blog.image}
+              />
+            </motion.div>
           );
         })}
       </div>
--- Componets/Header.tsx
+++ Componets/Header.tsx
@@ -10,22 +10,25 @@ const Header = () => {
         <div className="flex items-center gap-1">
           <motion.div
             initial={{
-              x: -20,
+              x: -50,
               opacity: 0,
+              rotate: "-5deg",
             }}
             whileInView={{
               x: 0,
               opacity: 1,
+              rotate: 0,
             }}
             transition={{
-              duration: 1,
+              duration: 0.5,
             }}
           >
-            <Image
+            {/* <Image
               src={assets.logo}
               alt="logo"
               className="w-[60px] sm:w-[80px]"
-            />
+            /> */}
+            <h1 className="md:text-4xl text-xl font-semibold">Inkwell.</h1>
           </motion.div>
         </div>
 
@@ -34,23 +37,39 @@ const Header = () => {
             scale: 1.05,
             x: 50,
             rotate: "5deg",
-            transition: { duration: 0.1, delay: 0 },
+            transition: { duration: 0.1, type: "spring", delay: 0 },
           }}
-          className="flex cursor-pointer items-center gap-2 font-meduim py-1 px-3 sm:py-2 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]"
+          className="flex cursor-pointer text-sm md:text-base items-center gap-2 font-meduim py-1 px-3 sm:py-2 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]"
         >
           Get Started <Image src={assets.arrow} alt="" />
         </motion.button>
       </div>
       <div className="my-8 text-center">
-        <h1 className="text-3xl font-medium sm:text-5xl">Latest Blogs</h1>
-        <p className="max-w-2xl m-auto mt-10 text-xs sm:text-base">
+        <motion.h1
+          initial={{ opacity: 0, y: -50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+          className="text-3xl font-medium sm:text-5xl"
+        >
+          Latest Blogs
+        </motion.h1>
+        <motion.p
+          initial={{ opacity: 0, x: -50 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5 }}
+          className="max-w-2xl m-auto mt-10 text-xs sm:text-base"
+        >
           Stay updated with the latest insights and trends across Technology,
           Lifestyle, Education, Business and Politics through our most recent
           blog posts.
-        </p>
-        <form
+        </motion.p>
+        <motion.form
+          whileHover={{
+            scale: 1.2,
+            transition: { duration: 0.2 },
+          }}
           action=""
-          className=" shadow-[-7px_7px_0px_#000000] flex justify-between max-w-xl mx-auto mt-10 scale-75 border border-black sm:scale-75"
+          className="cursor-pointer shadow-[-7px_7px_0px_#000000] flex justify-between max-w-xl mx-auto mt-10 scale-75 border border-black sm:scale-75"
         >
           <input
             type="email"
@@ -63,7 +82,7 @@ const Header = () => {
           >
             Subscribe
           </button>
-        </form>
+        </motion.form>
       </div>
     </div>
   );
